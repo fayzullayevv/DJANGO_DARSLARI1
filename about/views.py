5cc2f1f592b8d47414f2d3947efb62ca74a18d1e
FaYozzz <120025589+FaYozzz@users.noreply.github.com>
2022-12-07 18:16:19 +0500

    Initial commit

---
+++ about/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def About(request):
+    return HttpResponse("Fayzullayev Fayozbek")
+
+# Create your views here.
---
+++ contact/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def Contact(request):
+    return HttpResponse("Nomer")
+# Create your views here.
